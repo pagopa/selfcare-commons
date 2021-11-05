f47940adcc779f24e453bab9102e226c6fbd4850
Rodolfo Viti <rodolfo.viti@nttdata.com>
2021-11-05 19:35:06 +0100

    removed unused lombok annotations

--- base/src/main/java/it/pagopa/selfcare/commons/base/security/SelfCareAuthenticationDetails.java
+++ base/src/main/java/it/pagopa/selfcare/commons/base/security/SelfCareAuthenticationDetails.java
@@ -1,12 +1,8 @@
 package it.pagopa.selfcare.commons.base.security;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@EqualsAndHashCode
-@ToString
 public class SelfCareAuthenticationDetails {
 
     private final String institutionId;
--- base/src/main/java/it/pagopa/selfcare/commons/base/security/SelfCareGrantedAuthority.java
+++ base/src/main/java/it/pagopa/selfcare/commons/base/security/SelfCareGrantedAuthority.java
@@ -1,15 +1,11 @@
 package it.pagopa.selfcare.commons.base.security;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.util.Assert;
 
 import java.util.Collection;
 import java.util.Set;
 
-@EqualsAndHashCode(of = "role")
-@ToString
 public class SelfCareGrantedAuthority implements GrantedAuthority {
 
     private final String role;
