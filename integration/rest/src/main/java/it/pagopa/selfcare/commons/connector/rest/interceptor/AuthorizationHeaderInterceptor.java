84ad6fd70d852ae971cd892a6f43b590daab0dbd
Rodolfo Viti <rodolfo.viti@nttdata.com>
2021-11-05 16:10:03 +0100

    fix AuthorizationHeaderInterceptor with RequestContextHolder

--- integration/rest/src/main/java/it/pagopa/selfcare/commons/connector/rest/interceptor/AuthorizationHeaderInterceptor.java
+++ integration/rest/src/main/java/it/pagopa/selfcare/commons/connector/rest/interceptor/AuthorizationHeaderInterceptor.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Slf4j
 public class AuthorizationHeaderInterceptor implements RequestInterceptor {
@@ -15,6 +17,13 @@ public class AuthorizationHeaderInterceptor implements RequestInterceptor {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null) {
             template.header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", authentication.getCredentials()));
+
+        } else if (RequestContextHolder.getRequestAttributes() != null
+                && ServletRequestAttributes.class.isAssignableFrom(RequestContextHolder.getRequestAttributes().getClass())) {
+            template.header(HttpHeaders.AUTHORIZATION,
+                    ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                            .getRequest()
+                            .getHeader(HttpHeaders.AUTHORIZATION));
         }
     }
 
