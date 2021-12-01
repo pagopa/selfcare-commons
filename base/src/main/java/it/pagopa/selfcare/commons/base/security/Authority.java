4c711d7858a64ccfbaf87fcd5d06244a33658277
Rodolfo Viti <rodolfo.viti@nttdata.com>
2021-12-01 19:00:16 +0100

    rework roles management

--- base/src/main/java/it/pagopa/selfcare/commons/base/security/Authority.java
+++ base/src/main/java/it/pagopa/selfcare/commons/base/security/Authority.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum Authority {
     ADMIN("Amministratore"),
+    LIMITED("Operatore"),
     LEGAL("Rappresentante Legale dell'ente"),
     ADMIN_REF("Referente Amministrativo"),
     TECH_REF("Referente Tecnico"),
