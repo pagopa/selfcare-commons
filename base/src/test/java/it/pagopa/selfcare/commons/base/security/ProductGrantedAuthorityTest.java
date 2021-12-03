b1c91b8969ae59200e7ee2d9b8bc5041c102ad05
Rodolfo Viti <rodolfo.viti@nttdata.com>
2021-12-03 23:27:29 +0100

    sonar fixes

--- base/src/test/java/it/pagopa/selfcare/commons/base/security/ProductGrantedAuthorityTest.java
+++ base/src/test/java/it/pagopa/selfcare/commons/base/security/ProductGrantedAuthorityTest.java
@@ -67,4 +67,16 @@ class ProductGrantedAuthorityTest {
         Assertions.assertNotEquals(authority3.hashCode(), authority.hashCode());
     }
 
+
+    @Test
+    void canEqual() {
+        // given
+        ProductGrantedAuthority authority1 = new ProductGrantedAuthority(SelfCareAuthority.LIMITED, "role1", "productId1");
+        ProductGrantedAuthority authority2 = new ProductGrantedAuthority(SelfCareAuthority.LIMITED, "role2", "productId2");
+        // when
+        boolean condition = authority1.canEqual(authority2);
+        // then
+        Assertions.assertTrue(condition);
+    }
+
 }
