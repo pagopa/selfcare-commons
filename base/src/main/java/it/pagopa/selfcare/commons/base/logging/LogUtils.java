8793ba413a11185b479b9bc1abcbf6bd49d61c77
Kevin Silva <84286727+KevinSi96@users.noreply.github.com>
2022-02-14 16:02:37 +0100

    Feature/selc 487 (#17)
    * refactor for log encaptulation
    
    * update version to 1.2.0
    
    * add private constructor to LogUtils
    
    * Revert "add private constructor to LogUtils"
    
    This reverts commit 054cddc9
    
    * Update base/src/main/resources/logback-spring.xml
    
    Co-authored-by: Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
    
    Co-authored-by: USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
    Co-authored-by: Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>

---
+++ base/src/main/java/it/pagopa/selfcare/commons/base/logging/LogUtils.java
@@ -0,0 +1,10 @@
+package it.pagopa.selfcare.commons.base.logging;
+
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+public abstract class LogUtils {
+
+    public static final Marker CONFIDENTIAL_MARKER = MarkerFactory.getMarker("CONFIDENTIAL");
+
+}
