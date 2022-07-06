c561b88c74022ff95d599562bb0eb9debc52ca17
Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
2022-07-06 15:16:06 +0200

    Bugfix/selc 1304 (#31)
    * update version to 2.0.1-SNAPSHOT
    
    * add PartyRole to SecurityContext
    
    * update version to 2.0.1
    
    * fix trigger branch name
    
    Co-authored-by: Rodolfo Viti <rodolfo.viti@nttdata.com>

--- base/src/test/java/it/pagopa/selfcare/commons/base/security/ProductGrantedAuthorityTest.java
+++ base/src/test/java/it/pagopa/selfcare/commons/base/security/ProductGrantedAuthorityTest.java
@@ -1,52 +1,53 @@
 package it.pagopa.selfcare.commons.base.security;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 import java.util.Collection;
 import java.util.List;
 
+import static it.pagopa.selfcare.commons.base.security.PartyRole.*;
+import static it.pagopa.selfcare.commons.base.security.SelfCareAuthority.ADMIN;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductGrantedAuthorityTest {
 
     @Test
-    void ProductGrantedAuthority_nullSelcRole() {
+    void ProductGrantedAuthority_nullRole() {
         // given
-        SelfCareAuthority selcRole = null;
+        PartyRole partyRole = null;
         Collection<String> productRoles = List.of("productRole");
         String productId = "productId";
         // when
-        Executable executable = () -> new ProductGrantedAuthority(selcRole, productRoles, productId);
+        Executable executable = () -> new ProductGrantedAuthority(partyRole, productRoles, productId);
         // then
-        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, executable);
-        assertEquals("A Self Care granted authority is required", e.getMessage());
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, executable);
+        assertEquals("A Party Role is required", e.getMessage());
     }
 
     @Test
     void ProductGrantedAuthority_nullProductRole() {
         // given
-        SelfCareAuthority selcRole = SelfCareAuthority.ADMIN;
+        PartyRole partyRole = MANAGER;
         Collection<String> productRoles = null;
         String productId = "productId";
         // when
-        Executable executable = () -> new ProductGrantedAuthority(selcRole, productRoles, productId);
+        Executable executable = () -> new ProductGrantedAuthority(partyRole, productRoles, productId);
         // then
-        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, executable);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, executable);
         assertEquals("At least one Product granted authority textual representation is required", e.getMessage());
     }
 
     @Test
     void ProductGrantedAuthority_nullProductCode() {
         // given
-        SelfCareAuthority selcRole = SelfCareAuthority.ADMIN;
+        PartyRole partyRole = MANAGER;
         Collection<String> productRoles = List.of("productRole");
         String productId = "";
         // when
-        Executable executable = () -> new ProductGrantedAuthority(selcRole, productRoles, productId);
+        Executable executable = () -> new ProductGrantedAuthority(partyRole, productRoles, productId);
         // then
-        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class, executable);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, executable);
         assertEquals("A Product id is required", e.getMessage());
     }
 
@@ -54,19 +55,19 @@ class ProductGrantedAuthorityTest {
     @Test
     void ProductGrantedAuthority() {
         // given
-        SelfCareAuthority selcRole = SelfCareAuthority.ADMIN;
+        PartyRole partyRole = MANAGER;
         Collection<String> productRoles = List.of("productRole");
         String productId = "productId";
         // when
-        ProductGrantedAuthority authority = new ProductGrantedAuthority(selcRole, productRoles, productId);
+        ProductGrantedAuthority authority = new ProductGrantedAuthority(partyRole, productRoles, productId);
         // then
-        assertEquals(selcRole.name(), authority.getAuthority());
+        assertEquals(ADMIN.name(), authority.getAuthority());
         assertIterableEquals(productRoles, authority.getProductRoles());
         assertEquals(productId, authority.getProductId());
-        ProductGrantedAuthority authority2 = new ProductGrantedAuthority(SelfCareAuthority.LIMITED, List.of("role2"), productId);
+        ProductGrantedAuthority authority2 = new ProductGrantedAuthority(OPERATOR, List.of("role2"), productId);
         assertEquals(authority2, authority);
         assertEquals(authority2.hashCode(), authority.hashCode());
-        ProductGrantedAuthority authority3 = new ProductGrantedAuthority(SelfCareAuthority.ADMIN, productRoles, "productId2");
+        ProductGrantedAuthority authority3 = new ProductGrantedAuthority(SUB_DELEGATE, productRoles, "productId2");
         assertNotEquals(authority3, authority);
         assertNotEquals(authority3.hashCode(), authority.hashCode());
     }
@@ -77,8 +78,8 @@ class ProductGrantedAuthorityTest {
         // given
         Collection<String> productRoles = List.of("productRole");
         String productId = "productId";
-        ProductGrantedAuthority authority1 = new ProductGrantedAuthority(SelfCareAuthority.ADMIN, productRoles, productId);
-        ProductGrantedAuthority authority2 = new ProductGrantedAuthority(SelfCareAuthority.LIMITED, productRoles, productId);
+        ProductGrantedAuthority authority1 = new ProductGrantedAuthority(MANAGER, productRoles, productId);
+        ProductGrantedAuthority authority2 = new ProductGrantedAuthority(OPERATOR, productRoles, productId);
         // when
         Executable executable = () -> ProductGrantedAuthority.MERGE.apply(authority1, authority2);
         // then
@@ -89,17 +90,17 @@ class ProductGrantedAuthorityTest {
     @Test
     void mergeProductGrantedAuthority() {
         // given
-        SelfCareAuthority selcRole = SelfCareAuthority.ADMIN;
+        PartyRole partyRole = MANAGER;
         String productRole1 = "productRole1";
         String productRole2 = "productRole2";
         String productId = "productId";
-        ProductGrantedAuthority authority1 = new ProductGrantedAuthority(selcRole, productRole1, productId);
-        ProductGrantedAuthority authority2 = new ProductGrantedAuthority(selcRole, productRole2, productId);
+        ProductGrantedAuthority authority1 = new ProductGrantedAuthority(partyRole, productRole1, productId);
+        ProductGrantedAuthority authority2 = new ProductGrantedAuthority(partyRole, productRole2, productId);
         // when
         ProductGrantedAuthority merged = ProductGrantedAuthority.MERGE.apply(authority1, authority2);
         // then
         assertEquals(productId, merged.getProductId());
-        assertEquals(selcRole.toString(), merged.getAuthority());
+        assertEquals(ADMIN.toString(), merged.getAuthority());
         assertIterableEquals(List.of(productRole1, productRole2), merged.getProductRoles());
     }
 
--- base/src/test/java/it/pagopa/selfcare/commons/base/security/SelfCareGrantedAuthorityTest.java
+++ base/src/test/java/it/pagopa/selfcare/commons/base/security/SelfCareGrantedAuthorityTest.java
@@ -1,6 +1,5 @@
 package it.pagopa.selfcare.commons.base.security;
 
-import it.pagopa.selfcare.commons.utils.TestUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
@@ -9,8 +8,11 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
+import static it.pagopa.selfcare.commons.base.security.PartyRole.MANAGER;
+import static it.pagopa.selfcare.commons.base.security.PartyRole.OPERATOR;
 import static it.pagopa.selfcare.commons.base.security.SelfCareAuthority.ADMIN;
 import static it.pagopa.selfcare.commons.base.security.SelfCareAuthority.LIMITED;
+import static it.pagopa.selfcare.commons.utils.TestUtils.mockInstance;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SelfCareGrantedAuthorityTest {
@@ -46,9 +48,9 @@ class SelfCareGrantedAuthorityTest {
         // given
         String institutionId = "institutionId";
         ProductGrantedAuthority productRole1 =
-                TestUtils.mockInstance(new ProductGrantedAuthority(ADMIN, List.of("productRole1"), "productId1"), 1);
+                mockInstance(new ProductGrantedAuthority(MANAGER, List.of("productRole1"), "productId1"), 1);
         ProductGrantedAuthority productRole2 =
-                TestUtils.mockInstance(new ProductGrantedAuthority(LIMITED, List.of("productRole2"), "productId2"), 2);
+                mockInstance(new ProductGrantedAuthority(OPERATOR, List.of("productRole2"), "productId2"), 2);
         Collection<ProductGrantedAuthority> roleOnProducts =
                 List.of(productRole1, productRole2);
         // when
@@ -65,9 +67,9 @@ class SelfCareGrantedAuthorityTest {
         // given
         String institutionId = "institutionId";
         ProductGrantedAuthority productRole1 =
-                TestUtils.mockInstance(new ProductGrantedAuthority(LIMITED, "productRole1", "productId1"), 1);
+                mockInstance(new ProductGrantedAuthority(OPERATOR, "productRole1", "productId1"), 1);
         ProductGrantedAuthority productRole2 =
-                TestUtils.mockInstance(new ProductGrantedAuthority(LIMITED, "productRole2", "productId2"), 2);
+                mockInstance(new ProductGrantedAuthority(OPERATOR, "productRole2", "productId2"), 2);
         Collection<ProductGrantedAuthority> roleOnProducts =
                 List.of(productRole1, productRole2);
         // when
