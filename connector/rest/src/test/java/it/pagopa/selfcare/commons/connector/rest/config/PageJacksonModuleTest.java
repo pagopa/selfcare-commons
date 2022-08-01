e5c887a9b37c551dfe8eaa5e7aeda8cdefa43f9c
Rodolfo Viti <rodolfo.viti@nttdata.com>
2022-08-01 16:21:08 +0200

    fix sonar test coverage

--- connector/rest/src/test/java/it/pagopa/selfcare/commons/connector/rest/config/PageJacksonModuleTest.java
+++ connector/rest/src/test/java/it/pagopa/selfcare/commons/connector/rest/config/PageJacksonModuleTest.java
@@ -3,9 +3,10 @@ package it.pagopa.selfcare.commons.connector.rest.config;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.Page;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class PageJacksonModuleTest {
 
@@ -20,14 +21,24 @@ class PageJacksonModuleTest {
         String src = "{\"content\":[\"test\"],\"totalElements\":1,\"number\":0,\"size\":1}";
         Page<String> read = objectMapper.readValue(src, new TypeReference<>() {
         });
-        Assertions.assertNotNull(read);
-        Assertions.assertNotNull(read.getContent());
-        Assertions.assertEquals(1, read.getContent().size());
-        Assertions.assertEquals("test", read.getContent().get(0));
-        Assertions.assertEquals(0, read.getNumber());
-        Assertions.assertEquals(1, read.getTotalElements());
-        Assertions.assertEquals(1, read.getTotalPages());
-        Assertions.assertEquals(1, read.getSize());
+        assertNotNull(read);
+        assertNotNull(read.getContent());
+        assertEquals(1, read.getContent().size());
+        assertEquals("test", read.iterator().next());
+        assertEquals(0, read.getNumber());
+        assertEquals(1, read.getTotalElements());
+        assertEquals(1, read.getTotalPages());
+        assertEquals(1, read.getSize());
+        assertEquals(1, read.getNumberOfElements());
+        assertTrue(read.hasContent());
+        assertNotNull(read.getSort());
+        assertTrue(read.isFirst());
+        assertTrue(read.isLast());
+        assertFalse(read.hasNext());
+        assertFalse(read.hasPrevious());
+        assertNotNull(read.nextPageable());
+        assertNotNull(read.previousPageable());
+        assertEquals("pippo", read.map(s -> "pippo").iterator().next());
     }
 
 }
