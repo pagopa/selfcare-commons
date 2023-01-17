285707bd7ea01524d5773870133385d2d746d829
anttorre <antonio.torre@nttdata.com>
2023-01-17 15:23:38 +0100

    local private and cert loaded from properties

--- utils/crypto/src/main/java/it/pagopa/selfcare/commons/utils/crypto/config/LocalCryptoConfig.java
+++ utils/crypto/src/main/java/it/pagopa/selfcare/commons/utils/crypto/config/LocalCryptoConfig.java
@@ -1,42 +1,37 @@
 package it.pagopa.selfcare.commons.utils.crypto.config;
 
+import it.pagopa.selfcare.commons.utils.crypto.utils.CryptoUtils;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
-import java.io.FileInputStream;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
-import java.util.Enumeration;
 
-/** {@link Configuration} class to read private and public key to use when performing crypto operations */
+/**
+ * {@link Configuration} class to read private and public key to use when performing crypto operations
+ */
 @Conditional(LocalCryptoCondition.class)
 @Configuration
 @Getter
 public class LocalCryptoConfig {
 
-    private final PrivateKey privateKey;
     private final Certificate certificate;
+    private final PrivateKey privateKey;
 
-    public LocalCryptoConfig() {
+    public LocalCryptoConfig(
+            @Value("${crypto.key.cert}") String cert,
+            @Value("${crypto.key.private}") String pKey
+    ) {
         try {
-            char[] password = "idpay".toCharArray();
-
-            KeyStore keystore = KeyStore.getInstance("PKCS12");
-            keystore.load(new FileInputStream("src/test/resources/keystore.p12"), password);
-
-            Enumeration<String> aliases = keystore.aliases();
-            String alias;
-            if (aliases.hasMoreElements()) {
-                alias = aliases.nextElement();
-            } else {
-                throw new KeyStoreException("Keystore is empty");
+            if(!StringUtils.hasText(cert) || !StringUtils.hasText(pKey)){
+                throw new IllegalStateException("Define private and cert values in order to perform locally sign operations");
             }
-            privateKey = (PrivateKey) keystore.getKey(alias, password);
-            Certificate[] certificateChain = keystore.getCertificateChain(alias);
-            certificate = certificateChain[0];
+
+            certificate = CryptoUtils.getCertificate(cert);
+            privateKey = CryptoUtils.getPrivateKey(pKey);
         } catch (Exception e) {
             throw new IllegalStateException("Something gone wrong while loading crypto private and public keys", e);
         }
--- utils/crypto/src/main/java/it/pagopa/selfcare/commons/utils/crypto/utils/CryptoUtils.java
+++ utils/crypto/src/main/java/it/pagopa/selfcare/commons/utils/crypto/utils/CryptoUtils.java
@@ -1,9 +1,19 @@
 package it.pagopa.selfcare.commons.utils.crypto.utils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 
 public final class CryptoUtils {
     private CryptoUtils(){}
@@ -17,5 +27,32 @@ public final class CryptoUtils {
         } catch (IOException e) {
             throw new IllegalArgumentException("Something gone wrong while reading inputStream", e);
         }
+    }
+
+    public static X509Certificate getCertificate(String cert) throws IOException, CertificateException {
+        try(
+                InputStream is = new ByteArrayInputStream(cert.getBytes(StandardCharsets.UTF_8))
+        ) {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            return (X509Certificate) cf.generateCertificate(is);
+        }
+    }
+
+    public static RSAPrivateKey getPrivateKey(String privateKey) throws InvalidKeySpecException, NoSuchAlgorithmException, IOException {
+        String keyStringFormat =  pemToString(privateKey);
+        try(
+                InputStream is = new ByteArrayInputStream(Base64.getDecoder().decode(keyStringFormat))
+        ) {
+            PKCS8EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(is.readAllBytes());
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            return (RSAPrivateKey) kf.generatePrivate(encodedKeySpec);
+        }
+    }
+
+    public static String pemToString(String target) {
+        return target
+                .replaceAll("^-----BEGIN[A-Z|\\s]+-----", "")
+                .replaceAll("\\n+", "")
+                .replaceAll("-----END[A-Z|\\s]+-----$", "");
     }
 }
