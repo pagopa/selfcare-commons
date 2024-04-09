85c027a19f36cd7c2d5dde30a23e0ae67e8c70fd
manuraf <manuel.rafeli@pagopa.it>
2024-04-09 12:22:32 +0200

    container_app_environment_name as variable

--- infra/terraform-modules/container_app_microservice/locals.tf
+++ infra/terraform-modules/container_app_microservice/locals.tf
@@ -6,7 +6,7 @@ locals {
   vnet_name                               = "${local.project}-vnet-rg"
   key_vault_resource_group_name           = "${local.project}${local.pnpg_suffix}-sec-rg"
   key_vault_name                          = "${local.project}${local.pnpg_suffix}-kv"
-  container_app_environment_name          = "${local.project}${local.pnpg_suffix}-cae"
+  container_app_environment_name          = var.container_app_environment_name
   container_name                          = "${local.project}${local.pnpg_suffix}-${var.container_app_name}"
   app_name                                = "${local.container_name}-ca"
   container_app_environment_dns_zone_name = "azurecontainerapps.io"
