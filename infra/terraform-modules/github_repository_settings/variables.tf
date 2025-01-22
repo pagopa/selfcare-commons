d2d10a4c48df7fcab3c4c55b8a966e1808b08203
AleDore <alessio85.dore@gmail.com>
2025-01-22 12:16:58 +0100

    feat: Add Front End managed identity on CI/CD

--- infra/terraform-modules/github_repository_settings/variables.tf
+++ infra/terraform-modules/github_repository_settings/variables.tf
@@ -3,3 +3,12 @@ variable "github" {
     repository = string
   })
 }
+
+variable "identity_component" {
+  type        = string
+  default     = "ms"
+  validation {
+    condition     = contains(["ms", "fe"], var.identity_component)
+    error_message = "The identity_component variable must be either 'ms' or 'fe'."
+  }
+}
