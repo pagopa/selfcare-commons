1682250e3f66f40caff58dba4aed061b453e937a
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-01-27 16:04:28 +0100

    [SELC-8184] Using io.swagger.v3.oas.annotations in Page and Problem classes

--- web/src/main/java/it/pagopa/selfcare/commons/web/model/Page.java
+++ web/src/main/java/it/pagopa/selfcare/commons/web/model/Page.java
@@ -1,36 +1,40 @@
 package it.pagopa.selfcare.commons.web.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
 import java.util.List;
 
 @Data
 public class Page<T> {
 
-    @ApiModelProperty(value = "The page content", required = true)
+    @Schema(description = "The page content")
     @JsonProperty(required = true)
     @NotNull
     @Valid
     private List<T> content;
 
-    @ApiModelProperty(value = "The total amount of elements", required = true)
+    @Schema(description = "The total amount of elements")
     @JsonProperty(required = true)
+    @NotNull
     private long totalElements;
 
-    @ApiModelProperty(value = "The number of total pages", required = true)
+    @Schema(description = "The number of total pages")
     @JsonProperty(required = true)
+    @NotNull
     private int totalPages;
 
-    @ApiModelProperty(value = "The number of the current page", required = true)
+    @Schema(description = "The number of the current page")
     @JsonProperty(required = true)
+    @NotNull
     private int number;
 
-    @ApiModelProperty(value = "The size of the page", required = true)
+    @Schema(description = "The size of the page")
     @JsonProperty(required = true)
+    @NotNull
     private int size;
 
 }
